c5806eb836fd0c1ee0040d7a9d8fac7ff4597cfe
Pol Llovet <pol.llovet@gmail.com>
2011-11-03 13:27:54 -0600

    Initial commit of app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sapphire::Application.config.secret_token = '4a6d5866e55082b84974ffb5930ca0633fe7bb66f219ce297f39d5d94b6c3b54afd936c844291df74dd61bb2da9768f4c5e20266cd7ab2dfff60a47e902b81b8'
