475661dc1913963fcda33a63b4eadaf4c1f11326
Pol Llovet <pol.llovet@gmail.com>
2012-02-01 11:41:57 -0700

    Add project and data collection routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Sapphire::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :projects
+  resources :data_collections
 
   # Sample resource route with options:
   #   resources :products do
@@ -49,6 +51,7 @@ Sapphire::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'projects#index'
 
   # See how all your routes lay out with "rake routes"
 
