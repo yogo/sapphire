94034ae89b7de72411c2c987e0ea58068640dffb
Pol Llovet <pol.llovet@gmail.com>
2012-02-01 12:21:16 -0700

    Add project creation features

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,4 +16,18 @@ class ProjectsController < ApplicationController
       @project = Yogo::Project.get(params[:id])
       @project.merge(params[:project])
     end
+    
+    def new
+      @project = Yogo::Project.new
+    end
+    
+    def create
+      @project = Yogo::Project.new(params[:yogo_project])
+      if @project.save
+        redirect_to projects_path
+      else
+        flash[:error] = "Project failed to save!"
+        render :new
+      end
+    end
 end
