0b271352dce68035581bb5b5c265d4de736584fd
Pol Llovet <pol.llovet@gmail.com>
2012-02-03 17:42:44 -0700

    Add schemas to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Sapphire::Application.routes.draw do
 
   resources :projects do 
-    resources :collections
+    resources :collections do
+      resources :schemas
+    end
   end
 
   root :to => 'projects#index'
