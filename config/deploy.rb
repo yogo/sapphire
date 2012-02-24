879975df8b14580cdef66cf213e262c0ff489baf
Pol Llovet <pol.llovet@gmail.com>
2012-02-24 16:49:16 -0700

    Deployment: working!

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,68 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, 'ruby-1.9.2-p180@passenger'
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+require "bundler/capistrano"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :application, "sapphire"
+set :scm, :git
+set :repository,  "git://github.com/yogo/sapphire.git"
+set :branch, "rails_3.0.5"
+set :deploy_via, :remote_cache
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+role :web, "sapphire.rcg.montana.edu"                          # Your HTTP server, Apache/etc
+role :app, "sapphire.rcg.montana.edu"                          # This may be the same as your `Web` server
+role :db,  "sapphire.rcg.montana.edu", :primary => true # This is where Rails migrations will run
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :use_sudo, false
+set :user, "sapphire"
+set :deploy_to, "/home/sapphire/rails"
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+set :links, {
+  :files => %w{
+    config/database.yml
+  },
+  :dirs => %w{
+    public/asset_collection
+    public/uploads
+  }
+}
+
+namespace :sapphire do
+  
+  namespace :db do
+    task :setup do
+      run "cd #{current_path} && RAILS_ENV=production bundle exec rake db:setup"
+    end
+
+    task :autoupgrade do
+      run "cd #{current_path} && RAILS_ENV=production bundle exec rake db:autoupgrade"
+    end
+  end
+  
+  namespace :symlink do
+    task :setup do
+      links[:dirs].each do |l|
+        run "mkdir -p #{deploy_to}/#{shared_dir}/#{l}"
+      end      
+    end
+
+    task :link do
+      (links[:files] + links[:dirs]).each do |l|
+        run "ln -nfs #{deploy_to}/#{shared_dir}/#{l} #{release_path}/#{l}"
+      end
+    end
+  end
+end
+
+after "deploy:setup",       "sapphire:symlink:setup"
+after "deploy:symlink",     "sapphire:symlink:link"
+after "deploy:update_code", "sapphire:db:autoupgrade"
