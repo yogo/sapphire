ce98ec9c2a052bdaf4677572b2f9349a440c7084
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-02-29 13:54:12 -0700

    added a :method option for the nav_button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   ActionView::Base.default_form_builder = SapphireFormBuilder
   
-  def nav_button(str, img, href)
+  def nav_button(str, img, href, method="get")
     button = "<button class='button'>#{image_tag(img) } #{str}</button>".html_safe
-    link_to(button, href)
+    link_to(button, href, :method=>method.to_sym)
   end
   
 end
