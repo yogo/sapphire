17581b548abeb8f6c1bdff8218602764ada84fcc
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-03-02 08:50:45 -0700

    added category asssignment for a collection

---
+++ app/helpers/collections_helper.rb
@@ -0,0 +1,7 @@
+module CollectionsHelper
+  
+  def collection_category_options()
+    category_array = [["None",""],["Controlled Vocabulary", "Controlled Vocabulary"],["Schema Controlled Vocabulary","Schema Controlled Vocabulary"]]
+  end
+  
+end
