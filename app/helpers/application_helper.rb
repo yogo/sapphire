94587316d9623343b479faf116fda10c54aede73
Pol Llovet <pol.llovet@gmail.com>
2012-03-02 19:07:05 -0700

    Add helpers for the new public/private items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,22 @@
 module ApplicationHelper
   ActionView::Base.default_form_builder = SapphireFormBuilder
   
-  def nav_button(str, img, href, method="get")
+  def nav_button(str, img, href, method=nil)
     button = "<button class='button'>#{image_tag(img) } #{str}</button>".html_safe
-    link_to(button, href, :method=>method.to_sym)
+    link_to(button, href, :method=>method)
+  end
+
+  def my_project?(project, response = '')
+    if project && project.members.include?(current_user)
+      return response
+    end
+    return ''
   end
   
+  def public_project?(project, response = '')
+    if project && project.public?
+      return response
+    end
+    return ''
+  end
 end
