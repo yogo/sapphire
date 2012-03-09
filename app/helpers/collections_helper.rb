f37e8e8ece1113b518cfdcc25f94013bf11bc523
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-03-09 16:08:51 -0700

    updated Category and CV managment

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,7 +1,7 @@
 module CollectionsHelper
   
   def collection_category_options()
-    category_array = [["None",""],["Controlled Vocabulary", "Controlled Vocabulary"],["Schema Controlled Vocabulary","Schema Controlled Vocabulary"]]
+    category_array = [["None",""],["Controlled Vocabulary", "Controlled Vocabulary"]]
   end
   
 end
