7f4b5da676e485f8c749860c72664bc4915a24ff
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-03-14 17:04:34 -0600

    added support for raw DataMapper queries and parsing their structs to hash and json

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,25 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Sapphire::Application.initialize!
+
+#extend the Struct class
+class Struct
+   def to_hash
+     Hash[*members.zip(values).flatten]
+   end
+ end
+#extend Array so that raw sql results can be converted to json easily
+#assumes this is an array of Structs or Objects that have the to_hash method
+class Array
+  def sql_to_json
+    self.map{|k| k.to_hash}.to_json
+  end
+end
+
+module DataMapper
+  def self.raw_select(dm_query)
+    statement, bind_vars = repository.adapter.send(:select_statement, dm_query.query)
+    sql = repository.adapter.send(:open_connection).create_command(statement).send(:escape_sql, bind_vars)
+    repository.adapter.select(sql)
+  end
+end
