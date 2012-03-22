0d34a8295d6be9936abc0b3e4a4fb182057bfe6a
Pol Llovet <pol.llovet@gmail.com>
2012-03-22 14:22:28 -0600

    Add tab functionality to the containers

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+  changeTab = function(tab, coll) {
+    // set tabs to inactive
+    $(".block .secondary-navigation li").removeClass('active');
+    // hide all section
+    $(".block .collection").hide();
+    // activate the new tab
+    $(".block .secondary-navigation " + tab).addClass('active');
+    // show the section
+    $(".block " + coll).show();
+  };
