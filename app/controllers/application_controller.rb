16e8d527cc3a08709744a19ec9ed81576b9259a3
Pol Llovet <pol.llovet@gmail.com>
2012-11-19 00:10:48 -0700

    Finished cleaning up styles
    - projects#index, #new done.
    - removed the form builder (unnecessary)
    - removed the box layout (unnecessary)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
-  before_filter :choose_layout
-  layout :choose_layout
-
-  def choose_layout
-    if %w{sessions registrations passwords}.include?(controller_name)
-      return 'box'
-    end
-    return 'application'
-  end
 end
