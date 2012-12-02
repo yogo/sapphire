91eebcd63777c1c5c6f1ad16176b7fe3ae498758
Pol Llovet <pol.llovet@gmail.com>
2012-12-01 21:34:38 -0700

    Change the schema types and add help text.

--- app/helpers/schemas_helper.rb
+++ app/helpers/schemas_helper.rb
@@ -1,10 +1,34 @@
 module SchemasHelper
   def property_types_opts
-    Yogo::Collection::Property::COMMON_PROPERTIES.map(&:to_s).zip(
+    types = Yogo::Collection::Property::COMMON_PROPERTIES.map(&:to_s).zip(
       Yogo::Collection::Property::COMMON_PROPERTIES.map{|p| "Yogo::Collection::Property::" + p.to_s}
-    )
+    ) + [ 
+      ['Prompt', 'controlled_vocabulary'],
+      ['Link', 'association'],
+      ['Link List', 'list_association']
+    ]
+
+    types.map do |type|
+      key = case type[0]
+      when 'String'   then 'Short Text'
+      when 'Text'     then 'Long Text'
+      when 'Boolean'  then 'True/False'
+      when 'Integer'  then 'Whole Number'
+      when 'Float'    then 'Decimal Number'
+      when 'DateTime' then 'Date & Time'
+      else
+        type[0]
+      end
+      [key,type[1]]
+    end
+
   end
   
+  def position_opts(coll)
+    len = coll.schema.count 
+    (1..len+1).to_a.zip((0..len)).reverse
+  end
+
   def controlled_vocabulary_opts
     project_ids= current_user.memberships(:fields=>[:project_id]).map{|m| m.project_id}.uniq
     my_collection_ids = Yogo::Collection::Data.all(:project_id=> project_ids, :category=>"Controlled Vocabulary").map{|c| c.id}
