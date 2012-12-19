035d714f06e894741e813da78ee314c41177f3a8
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-12-19 16:29:50 -0700

    added a helper for getting all File collection options for selectbox

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -7,4 +7,8 @@ module ItemsHelper
   def associated_field_value_options(schema)
     schema.data_collection.items.map{|i| [i[schema.name],'{"project":{"id": "'+schema.data_collection.project_id+'"}, "collection":{"id": "'+schema.data_collection.id+'"},"item":{"id": "'+i.id.to_s+'", "display": "'+i[schema.name]+'"}}'] }
   end
+  
+  def associated_file_options(file_collection)
+    file_collection.items.map{|i| [i.original_filename.to_s,'{"project":{"id": "'+file_collection.project_id+'"}, "collection":{"id": "'+file_collection.id+'"},"item":{"id": "'+i.id.to_s+'", "display": "'+i.original_filename.to_s+'"}}'] }
+  end
 end
