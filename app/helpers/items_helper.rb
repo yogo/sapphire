e5c43de5b2c101670671f23da47f77de0f2858e2
Sean Cleveland <sean.b.cleveland@gmail.com>
2013-01-04 16:15:06 -0700

    updated the filtering and helpers to be faster

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -5,15 +5,7 @@ module ItemsHelper
   end
   
   def associated_field_value_options(schema)
-    project_id = schema.data_collection.project_id
-    collection_id = schema.data_collection.id
-    #schema_name = schema.name
-    schema.data_collection.items_json_array(schema.field_name, project_id, collection_id)
-    #    items = DataMapper.raw_select(schema.data_collection.items)
-
-    # items.map do |i| 
-    #   [i[schema_name],i.id.to_s]
-    # end#'{"project":{"id": "'+project_id+'"}, "collection":{"id": "'+collection_id+'"},"item":{"id": "'+i.id.to_s+'", "display": "'+i[schema_name]+'"}}'] }
+    schema.data_collection.items_json_array(schema.field_name, schema.data_collection.project_id, schema.data_collection.id)
   end
   
   def associated_field_display_options(schema)
@@ -21,6 +13,6 @@ module ItemsHelper
   end
   
   def associated_file_options(file_collection)
-    file_collection.items.map{|i| [i.original_filename.to_s,'{"project":{"id": "'+file_collection.project_id+'"}, "collection":{"id": "'+file_collection.id+'"},"item":{"id": "'+i.id.to_s+'", "display": "'+i.original_filename.to_s+'"}}'] }
+    file_collection.files_array(file_collection.project_id, file_collection.id)
   end
 end
--- lib/yogo/collection_ext.rb
+++ lib/yogo/collection_ext.rb
@@ -12,20 +12,32 @@ module Yogo
       
       #return an array of [item value, item id]
       def items_array(field)
-        DataMapper.raw_select(self.items).map{|i| [i[field], i.id.to_s]}
+        DataMapper.raw_select(self.items).map{|i| [(i[field].nil? ? "" : i[field]), i.id.to_s]}
       end
       
       #return an array of [item value, item value]
       def items_value_array(field)
-        DataMapper.raw_select(self.items).map{|i| [i[field], i[field]]}
+        DataMapper.raw_select(self.items).map{|i| [(i[field].nil? ? "" : i[field]), (i[field].nil? ? "" : i[field])]}
       end
       
       #return a array of [item value, json_association] for use in 
       def items_json_array(field, project_id, collection_id)
-        DataMapper.raw_select(self.items).map{|i| [i[field], '{"project":{"id": "'+project_id+'"}, "collection":{"id": "'+collection_id+'"},"item":{"id": "'+i.id.to_s+'", "display": "'+i[field]+'"}}'] }
+        items = DataMapper.raw_select(self.items).map{|i| [i[field], '{"project":{"id": "'+project_id+'"}, "collection":{"id": "'+collection_id+'"},"item":{"id": "'+i.id.to_s+'", "display": "'+(i[field].nil? ? "" : i[field])+'"}}'] }
       end
       def public?
         !private?
+      end
+    end
+  
+    class Asset
+      #return an array of [item value, item id]
+      def files_json_array(project_id, collection_id)
+        DataMapper.raw_select(self.items).map{|i| [i.original_filename.to_s,'{"project":{"id": "'+project_id+'"}, "collection":{"id": "'+collection_id+'"},"item":{"id": "'+i.id.to_s+'", "display": "'+i.original_filename.to_s+'"}}'] }
+      end
+      
+      
+      def files_array(project_id, collection_id)
+        DataMapper.raw_select(self.items).map{|i| [i.original_filename.to_s,i.original_filename.to_s] }
       end
     end
   end
