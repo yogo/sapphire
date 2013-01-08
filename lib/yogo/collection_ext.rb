5fd1f42aed6d1d1ad6ba97190ea8875eefadb614
Sean Cleveland <sean.b.cleveland@gmail.com>
2013-01-08 12:07:16 -0700

    removed all datamapper.raw_selects for now

--- lib/yogo/collection_ext.rb
+++ lib/yogo/collection_ext.rb
@@ -12,17 +12,24 @@ module Yogo
       
       #return an array of [item value, item id]
       def items_array(field)
-        DataMapper.raw_select(self.items).map{|i| [(i[field].nil? ? "" : i[field]), i.id.to_s]}
+        self.items.map{|i| [(i[field].nil? ? "" : i[field]), i.id.to_s]}
       end
       
       #return an array of [item value, item value]
       def items_value_array(field)
-        DataMapper.raw_select(self.items).map{|i| [(i[field].nil? ? "" : i[field]), (i[field].nil? ? "" : i[field])]}
+        self.items.map{|i| [(i[field].nil? ? "" : i[field]), (i[field].nil? ? "" : i[field])]}
       end
       
       #return a array of [item value, json_association] for use in 
       def items_json_array(field, project_id, collection_id)
-        items = DataMapper.raw_select(self.items).map{|i| [i[field], '{"project":{"id": "'+project_id+'"}, "collection":{"id": "'+collection_id+'"},"item":{"id": "'+i.id.to_s+'", "display": "'+(i[field].nil? ? "" : i[field])+'"}}'] }
+        #pql= "SELECT * FROM pg_prepared_xacts"
+        #repository.adapter.select(pql)
+        #sql = "Select #{field}, id FROM '#{collection_id.to_s.gsub('-','_')}s'"
+        #debugger
+        #results = repository.adapter.select(sql)
+        #debugger
+        #self.items(:fields=>["#{field}", :id])
+        self.items.map{|i| [i[field], '{"project":{"id": "'+project_id+'"}, "collection":{"id": "'+collection_id+'"},"item":{"id": "'+i.id.to_s+'", "display": "'+(i[field].nil? ? "" : i[field])+'"}}'] }
       end
       def public?
         !private?
@@ -32,12 +39,12 @@ module Yogo
     class Asset
       #return an array of [item value, item id]
       def files_json_array(project_id, collection_id)
-        DataMapper.raw_select(self.items).map{|i| [i.original_filename.to_s,'{"project":{"id": "'+project_id+'"}, "collection":{"id": "'+collection_id+'"},"item":{"id": "'+i.id.to_s+'", "display": "'+i.original_filename.to_s+'"}}'] }
+        self.items.map{|i| [i.original_filename.to_s,'{"project":{"id": "'+project_id+'"}, "collection":{"id": "'+collection_id+'"},"item":{"id": "'+i.id.to_s+'", "display": "'+i.original_filename.to_s+'"}}'] }
       end
       
       
       def files_array(project_id, collection_id)
-        DataMapper.raw_select(self.items).map{|i| [i.original_filename.to_s,i.original_filename.to_s] }
+        self.items.map{|i| [i.original_filename.to_s,i.original_filename.to_s] }
       end
     end
   end
