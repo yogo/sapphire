2f5c67ac309c7eddf2ef8770566f699c986c6d54
Sean Cleveland <sean.b.cleveland@gmail.com>
2013-09-23 13:20:52 -0600

    added a predicate helper

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -4,6 +4,10 @@ module ItemsHelper
     schema.data_collection.items.map{|i| [i[schema.name],i[schema.name]] }
   end
   
+  def predicate_options()
+    predicates = Array.new([["is equal to","="],["is like","like"],["is greater than","gt"], ["is greater than or equal to","gte"],["is less than","<"],["is less than or equal to", "<="]])
+  end
+  
   def associated_field_value_options(schema)
     schema.data_collection.items_json_array(schema.field_name, schema.data_collection.project_id, schema.data_collection.id)
   end
